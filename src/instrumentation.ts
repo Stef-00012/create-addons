e257890d70fa0a1a4556e5098aeafb79630da610
Stef-00012 <admin@stefdp.com>
2025-04-11 14:10:17 +0200

    add env

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,7 +1,9 @@
 import { startScheduler } from "./lib/scheduler";
 
 export function register() {
-    console.log("test")
-
-    startScheduler();
+    if (process.env.ENVIRONMENT === "production") {
+        startScheduler();
+    } else {
+        console.log("Fetching is not started in development mode");
+    }
 }
