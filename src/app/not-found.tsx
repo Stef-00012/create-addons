bae5cfa24096fe813324b077df78811cd334d55a
Stef-00012 <me@stefdp.com>
2025-06-07 14:13:21 +0200

    update 404 redirect

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,11 +1,13 @@
+import Link from "next/link";
+
 export default function NotFound() {
 	return (
 		<div className="mx-auto max-w-4xl mt-[30vh] sm:mt-[40vh]">
 			<h1>
 				error 404 — you&apos;re lost, here&apos;s a link to the{" "}
-				<a href="../" className="link2">
+				<Link href="/" className="link2">
 					main page
-				</a>
+				</Link>
 			</h1>
 		</div>
 	);
